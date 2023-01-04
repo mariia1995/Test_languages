73b6a615de5764c08da6dd36a1558043b4ad70ca
mariia1995 <89608477912@yandex.ru>
2023-01-04 14:50:59 +0600

    task for lang

---
+++ test_items.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_lan(browser):
+    browser.get(link)
+    time.sleep(5)
+    assert browser.find_element(By.CSS_SELECTOR, "[class='btn btn-lg btn-primary btn-add-to-basket'"), 'There is no button to add the product into the basket'
+    
+    
