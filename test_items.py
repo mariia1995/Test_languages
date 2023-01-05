3e8992d7cb53accb4ad0a701b4f529936556ffef
mariia1995 <89608477912@yandex.ru>
2023-01-05 13:33:00 +0600

    change of name

--- test_items.py
+++ test_items.py
@@ -4,7 +4,7 @@ import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_lan(browser):
+def test_different_languages(browser):
     browser.get(link)
     time.sleep(5)
     assert browser.find_element(By.CSS_SELECTOR, "[class='btn btn-lg btn-primary btn-add-to-basket'"), 'There is no button to add the product into the basket'
